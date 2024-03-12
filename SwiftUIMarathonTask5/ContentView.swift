9f6c571ce8530f5b7c7d8f280f35edaa6cb253ae
justSmK <28645140+justSmK@users.noreply.github.com>
2024-03-12 12:41:51 +0300

    Implementation

--- SwiftUIMarathonTask5/ContentView.swift
+++ SwiftUIMarathonTask5/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var position = CGPoint(x: 200, y: 380)
+    @State private var squareColor: Color = .black
+    
+    private let colors: [Color] = [.white, .pink, .yellow, .black]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 0, content: {
+                ForEach(colors, id: \.self) { color in
+                    Rectangle()
+                        .fill(color)
+                }
+            })
+            
+            CustomRectangle()
+                .position(position)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ dragGestureValue in
+                            position = dragGestureValue.location
+                    })
+                )
         }
-        .padding()
+    }
+}
+
+struct CustomRectangle: View {
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .frame(width: 100, height: 100)
+            .foregroundStyle(.white)
+            .blendMode(.difference)
+            .overlay(
+                Group {
+                    RoundedRectangle(cornerRadius: 15)
+                        .blendMode(.hue)
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundStyle(.white).blendMode(.overlay)
+                    
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundStyle(.black).blendMode(.overlay)
+                }
+            )
     }
 }
 
